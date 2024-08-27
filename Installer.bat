c8c22fbef11351d6aaec6dc28b8b64af6c8d86fa
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-08-28 02:25:24 +0530

    installer file fixed and web title changed

--- Installer.bat
+++ Installer.bat
@@ -1,74 +1,77 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal
 
-@REM cd C:\
-
-:: URLs for the installers
-@REM set GIT_URL=https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe
-set PYTHON_URL=https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe
-@REM set GIT_REPO_URL=https://github.com/R3tr0gh057/LibraryManagementSystem.git
-@REM set Arduino_Driver=https://www.silabs.com/documents/public/software/CP210x_Windows_Drivers.zip
-
-@REM :: File names
-@REM set GIT_INSTALLER=Git-2.45.2-64-bit.exe
-set PYTHON_INSTALLER=python-3.11.4-amd64.exe
-
-@REM :: Download Git installer
-@REM echo Downloading Git...
-@REM powershell -Command "Invoke-WebRequest -Uri %GIT_URL% -OutFile %GIT_INSTALLER%"
-@REM if not exist %GIT_INSTALLER% (
-@REM     echo Failed to download Git installer.
-@REM )
-@REM echo Installing Git...
-@REM start /wait %GIT_INSTALLER% /VERYSILENT
-@REM if %errorlevel% neq 0 (
-@REM     echo Failed to install Git.
-@REM )
-
-@REM :: Add Git to PATH
-@REM set PATH=%PATH%;C:\Program Files\Git\cmd
-
-@REM :: Clone Git repository
-@REM echo Cloning Git repository...
-@REM git clone %GIT_REPO_URL%
-@REM if %errorlevel% neq 0 (
-@REM     echo Failed to clone Git repository.
-@REM )
-@REM cd LibraryManagementSystem
-
-:: Download Python installer
-echo Downloading Python...
-powershell -Command "Invoke-WebRequest -Uri %PYTHON_URL% -OutFile %PYTHON_INSTALLER%"
-if not exist %PYTHON_INSTALLER% (
-    echo Failed to download Python installer.
-)
-echo Installing Python...
-start /wait %PYTHON_INSTALLER% /quiet InstallAllUsers=1 PrependPath=1
-if %errorlevel% neq 0 (
-    echo Failed to install Python.
+:: Check if Python is installed
+python --version >nul 2>&1
+if %ERRORLEVEL% equ 0 (
+    echo Python is already installed.
+    echo Installing requirements...
+    goto InstallRequirements
+) else (
+    echo Python is not installed.
+    echo Installing Python...
 )
 
-:: Add Python to PATH
-set PATH=%PATH%;C:\Program Files\Python311\Scripts
-
-:: Check if pip is installed
-echo Checking for pip...
-pip --version
-if %errorlevel% neq 0 (
-    echo pip not found.
+:: Install Python from the Dependencies folder
+set "PYTHON_INSTALLER=Dependencies\python-installer.exe"
+if exist "%PYTHON_INSTALLER%" (
+    "%PYTHON_INSTALLER%" /quiet InstallAllUsers=1 PrependPath=1
+    if %ERRORLEVEL% neq 0 (
+        echo Error: Python installation failed.
+        pause
+        exit /b %ERRORLEVEL%
+    )
+) else (
+    echo Error: Python installer not found in Dependencies folder.
+    pause
+    exit /b 1
 )
 
-:: Install Python requirements
-echo Installing Python requirements...
-pip install -r requirements.txt
-if %errorlevel% neq 0 (
-    echo Failed to install Python requirements.
+:InstallRequirements
+:: Install the requirements using pip
+pip install -r Dependencies\requirements.txt
+if %ERRORLEVEL% neq 0 (
+    echo Error: Failed to install Python packages from requirements.txt.
+    pause
+    exit /b %ERRORLEVEL%
 )
 
-:: Installation successful
+:InstallDrivers
+:: Install the drivers from the drivers folder
+echo Installing drivers...
+set "DRIVERS_DIR=drivers"
+set "DRIVER1=ftdiport.inf"
+set "DRIVER2=ftdibus.inf"
+
+if exist "%DRIVERS_DIR%\%DRIVER1%" (
+    pnputil /add-driver "%DRIVERS_DIR%\%DRIVER1%" /install
+    if %ERRORLEVEL% neq 0 (
+        echo Error: Failed to install %DRIVER1%.
+        pause
+        exit /b %ERRORLEVEL%
+    )
+) else (
+    echo Error: %DRIVER1% not found in drivers folder.
+    pause
+    exit /b 1
+)
+
+if exist "%DRIVERS_DIR%\%DRIVER2%" (
+    pnputil /add-driver "%DRIVERS_DIR%\%DRIVER2%" /install
+    if %ERRORLEVEL% neq 0 (
+        echo Error: Failed to install %DRIVER2%.
+        pause
+        exit /b %ERRORLEVEL%
+    )
+) else (
+    echo Error: %DRIVER2% not found in drivers folder.
+    pause
+    exit /b 1
+)
+
 echo Installation successful!
-echo Installed Python packages:
-pip list
+echo The following packages were installed:
+pip freeze
 
-endlocal
 pause
+exit /b 0
